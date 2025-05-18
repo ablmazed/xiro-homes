1502888a819adf5d6a317055ca2a2daba54724d6
mazed <ablmazed@gmail.com>
2025-05-18 08:04:59 +0600

    update 6

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
               onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
             >
               <ChevronUp className="mr-2 h-4 w-4" />
-              Back to top
+              Back to tops
             </Button>
           </div>
 
