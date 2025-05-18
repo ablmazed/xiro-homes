71a31a4c1e6d6b4304cc51c05eed5e251e276948
mazed <ablmazed@gmail.com>
2025-05-18 07:57:51 +0600

    update 4

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="default" className="bg-yellow-500">
+        Click me
+      </Button>
+    </div>
+  )
+}
---
+++ lib/data.ts
@@ -0,0 +1,87 @@
+const data = {
+  headerMenus: [
+    {
+      name: 'Home',
+      href: '/',
+    },
+    {
+      name: 'Buy',
+      href: '/buy',
+    },
+    {
+      name: 'Sell',
+      href: '/sell',
+    },
+    {
+      name: 'Rend',
+      href: '/rent',
+    },
+    {
+      name: 'Dashboard',
+      href: '/dashboard',
+    },
+  ],
+  footerMenus: [
+    {
+      title: 'Join Us',
+      links: [
+        {
+          name: 'Become an Agent',
+          href: '/',
+        },
+        {
+          name: 'Get Referrals',
+          href: '/',
+        },
+        {
+          name: 'Careers',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: 'Find Us',
+      links: [
+        {
+          name: 'Contact Us',
+          href: '/',
+        },
+        {
+          name: 'Help Center',
+          href: '/',
+        },
+        {
+          name: 'Advertise',
+          href: '/',
+        },
+        {
+          name: 'Become a Lander Partner',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: 'Subsidiaries',
+      links: [
+        {
+          name: 'Rent',
+          href: '/faqs',
+        },
+        {
+          name: 'ApartmentGuide',
+          href: '/',
+        },
+        {
+          name: 'Bay Equity Home Loans',
+          href: '/',
+        },
+        {
+          name: 'Title Forward',
+          href: '/',
+        },
+      ],
+    },
+  ],
+}
+
+export default data
