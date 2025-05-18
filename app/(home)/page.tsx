c0cee09858aed8d7362504d967e9dc26178b4f40
mazed <ablmazed@gmail.com>
2025-05-18 10:32:26 +0600

    update 19

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,4 @@
 import HeroBanner from '@/components/shared/home/HeroBanner'
-import HomeCard from '@/components/shared/home/HomeService'
 import HomeService from '../../components/shared/home/HomeService'
 
 export default function Home() {
