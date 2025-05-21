f18b143189e4ba512989462c80f15f8c57dbdebb
mazed <ablmazed@gmail.com>
2025-05-21 23:49:13 +0600

    update 30

--- app/(root)/properties/page.tsx
+++ app/(root)/properties/page.tsx
@@ -1,40 +1,14 @@
-'use client'
-import { useState } from 'react'
-// import { Spinner } from '@heroui/spinner'
-
-import { Input } from '@/components/ui/input'
-import ForSale from '@/components/shared/properties/ForSale'
 import PropertyCard from '@/components/shared/properties/PropertyCard'
+import { getAllProperty } from '@/lib/actions/property.actions'
 
-export default function Properties() {
-  //   const [loading, setLoading] = useState()
-  const [searchTerm, setSearchTerm] = useState('')
+export default async function PropertyPage() {
+  const propertydata = await getAllProperty()
+
+  console.log('data from page', propertydata)
+
   return (
-    <main>
-      {/* {loading && (
-        <div className="absolute inset-0 z-10 flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm">
-          <Spinner />
-        </div>
-      )} */}
-      <div>
-        <div className="w-full px-4 py-2 flex flex-row items-center justify-between gap-4 border-t border-b border-gray-700">
-          {/* Search Bar */}
-          <div className="flex-grow md:w-auto relative">
-            <Input
-              placeholder="Search by Address, neighborhood, city, ZIP"
-              className="w-full md:w-96"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)} // Set search term
-            />
-          </div>
-
-          {/* For Sale Component */}
-          <ForSale />
-        </div>
-      </div>
-      <div className="">
-        <PropertyCard />
-      </div>
-    </main>
+    <div className="p-6">
+      <PropertyCard properties={propertydata.property} />
+    </div>
   )
 }
