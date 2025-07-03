de38e78b2e76eaa7be8845809bba2183b7889e24
mazed <ablmazed@gmail.com>
2025-07-03 16:34:01 +0600

    11 commit

--- components/shared/home/HomeService.tsx
+++ components/shared/home/HomeService.tsx
@@ -5,7 +5,7 @@ export default function HomeService() {
   return (
     <section className="px-20">
       <div className="flex py-10 items-center justify-center">
-        <h2 className="font-bold text-4xl ">Our Service</h2>
+        <h2 className="font-bold text-4xl ">Our Services</h2>
       </div>
       <div className="flex gap-3 items-center justify-center md:p-4 md:space-y-4 ">
         <HomeCard cards={data.homeCard} />
